8050bdfec9b8211714df603af60d5e4d88115d6c
samarth-baghel <samarth.baghel@walkingtree.tech>
2022-06-07 15:29:31 +0530

    changes in dropdown

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,5 +1,5 @@
 import { MapsAPILoader } from '@agm/core';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 // import { MapComponent } from '../map/map.component';
 // import { PlaceholderDirective } from '../placeholder.directive';
@@ -10,7 +10,7 @@ import { CountryDetailsService } from '../service/country-details.service';
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss']
 })
-export class CountryComponent implements OnInit {
+export class CountryComponent implements OnInit, AfterViewInit  {
   country: any;
   isShown: boolean = true;
   searchDropdown: any;
@@ -18,13 +18,15 @@ export class CountryComponent implements OnInit {
   isCheck: boolean = true;
   isTrue: boolean = true;
   addres: any;
-  lat:any;
-  lng:any;
+  lat: any;
+  errorMsg: any = "";
+  lng: any;
   latitude: any = '';
   longitude: any = '';
+  isOptions: boolean = true
   // getAddress:any;
-  assgin:any;
-  currentLocation:any
+  assgin: any;
+  currentLocation: any
   // @ViewChild(PlaceholderDirective) mapHost!: PlaceholderDirective;
   countryName: any;
   geoCoder: any;
@@ -32,7 +34,17 @@ export class CountryComponent implements OnInit {
   constructor(
     // private componentFactory: ComponentFactoryResolver, 
     private countryDetails: CountryDetailsService,
+    private cdr : ChangeDetectorRef,
     private mapsAPILoader: MapsAPILoader) { }
+   
+  ngAfterViewInit(){
+    if (this.dataOfCountry.length == 0) {
+      console.log(this.dataOfCountry.length)
+      this.errorMsg = "There is no data in dropdown list";
+      this.cdr.detectChanges();
+    }
+
+  }
 
   ngOnInit(): void {
     // this.mapsAPILoader.load().then(() => {
@@ -40,13 +52,13 @@ export class CountryComponent implements OnInit {
     //   this.geoCoder = new google.maps.Geocoder;
     // });
     // this.get()
-    if(!navigator.geolocation){
+    if (!navigator.geolocation) {
       console.log("loaction is not find")
     }
-    navigator.geolocation.getCurrentPosition((postion:any)=>{
-      this.lat=postion.coords.latitude;
-      this.lng=postion.coords.longitude;
-      console.log(postion.coords.latitude,postion.coords.longitude)
+    navigator.geolocation.getCurrentPosition((postion: any) => {
+      this.lat = postion.coords.latitude;
+      this.lng = postion.coords.longitude;
+      console.log(postion.coords.latitude, postion.coords.longitude)
       // this.getReverseGeocodingData(postion.coords.latitude,postion.coords.longitude)
     })
     this.countryDetails.getDetailsOfCountry().subscribe((res) => {
@@ -63,12 +75,16 @@ export class CountryComponent implements OnInit {
       this.dataOfCountry.splice(indexOfIndia, 1);
       this.dataOfCountry.unshift("India");
       console.log(indexOfIndia);
-
+      console.log(this.countryName.length == 0)
+      console.log(this.dataOfCountry.length)
     })
+
   }
-  enabled(){
+  enabled() {
     this.isCheck = false;
     this.isTrue = false;
+    let devCn:any = document.querySelector('.ng-option .ng-option-disabled');
+    devCn.inneHtml="There is no country name in dropdown"
   }
   submit(formValue: NgForm) {
     this.isCheck = true
@@ -76,49 +92,75 @@ export class CountryComponent implements OnInit {
     console.log(this.country)
     // this.showMapComponent(this.country);
     for (let val of this.countryName) {
-      if(this.country=="India"){
-        this.latitude=this.lat;
-        this.longitude=this.lng;
+      if (this.country == "India" || this.searchDropdown == "India") {
+        this.latitude = this.lat;
+        this.longitude = this.lng;
       }
-     else if (val.name.common == this.country) {
+      else if (val.name.common == this.country || val.name.common == this.searchDropdown) {
         console.log(val.latlng)
         this.latitude = val.latlng[0];
         this.longitude = val.latlng[1]
         console.log(this.latitude)
         console.log(this.longitude)
-        
+
+      }else{
+        this.errorMsg="Plzs pay billing account of google console"
       }
     }
     this.countryName
   }
- 
 
+  move(event: any,val:any) {
+    console.log(event.keyCode);
+    if(event.keyCode==13){
+      this.submit(val);
+    }
+console.log(val)
+  }
   resetFormValue(formValue: NgForm) {
     formValue.resetForm();
-    this.isCheck=true
+    this.isCheck = true
     this.isTrue = true
     this.latitude = ''
     this.longitude = ''
     // this.mapHost.viewRefer.clear()
   }
-  // toShowDropdown() {
-  //   this.isShown = false
-  // }
-  // filtering() {
-  //   let textValue: any;
-  //   console.log(this.searchDropdown)
-  //   let options = document.querySelectorAll('option');
-  //   for (let i = 0; i < options.length; i++) {
-  //     textValue = options[i].value;
-  //     // textValue=options[i].innerText || options[i].textContent;
-  //     if (textValue.toUpperCase().indexOf(this.searchDropdown.toUpperCase()) > -1) {
-  //       options[i].style.display = "";
-  //     }
-  //     else {
-  //       options[i].style.display = "none";
-  //     }
-  //   }
-  // }
+  toShowDropdown() {
+    this.isShown = false
+  }
+  filtering(e:any) {
+    console.log(e.keyCode)
+    let textValue: any;
+    console.log(this.searchDropdown)
+    let options = document.querySelectorAll('option');
+    let optionCrt = document.createElement('option');
+    // optionCrt.value=""
+    let textNode = document.createTextNode("There is no country exits in dropdown");
+    optionCrt.appendChild(textNode);
+
+
+    let divCont: any = document.querySelector('.dropdown-content');
+    for (let i = 0; i < options.length; i++) {
+      textValue = options[i].value;
+      // textValue=options[i].innerText || options[i].textContent;
+      if (textValue.toUpperCase().indexOf(this.searchDropdown.toUpperCase()) == 0) {
+        // if(textValue.toUpperCase().indexOf(this.searchDropdown.toUpperCase()) == 1)
+        console.log(textValue.toUpperCase().indexOf(this.searchDropdown.toUpperCase()));
+        options[i].style.display = "";
+        divCont.style.left = "481"
+        divCont.style.height = "20vh";
+        divCont.style.width = "302px"
+        // divCont.style.overflow="auto"
+      }
+      else {
+
+        // options[i].innerText="There is no country exits in dropdown"
+        options[i].style.display = "none";
+        divCont.appendChild(optionCrt);
+        // optionCrt.innerText="";
+      }
+    }
+  }
   // private showMapComponent(urlOMap: any) {
   //   const mapComp = this.componentFactory.resolveComponentFactory(MapComponent);
   //   const placeHold = this.mapHost.viewRefer;
@@ -128,89 +170,89 @@ export class CountryComponent implements OnInit {
 
   // }
 
-  // setValue(val: any) {
-  //   this.isCheck = false;
-  //   this.isTrue = false;
-  //   this.isShown = true
-  //   console.log(val)
-  //   this.searchDropdown = val;
+  setValue(val: any) {
+    this.isCheck = false;
+    this.isTrue = false;
+    this.isShown = true
+    console.log(val)
+    this.searchDropdown = val;
 
+  }
+  //   getReverseGeocodingData(lat:any, lng:any) {
+  //     let latlng = {
+  //       lat:lat,
+  //       lng:lng
+  //     }
+  //     // let latlng = new google.maps.LatLng(lat, lng);
+  //     let obj:any={ 'latLng': latlng };
+  //     // This is making the Geocode request
+  //     let geocoder = new google.maps.Geocoder();
+  //     geocoder.geocode(obj, function (results, status) {
+  //         if (status !== google.maps.GeocoderStatus.OK) {
+  //             alert(status);
+  //         }
+  //         // This is checking to see if the Geoeode Status is OK before proceeding
+  //         if (status == google.maps.GeocoderStatus.OK) {
+  //             console.log(results);
+  //             var address = (results[0].formatted_address);
+  //         }
+  //     });
   // }
-//   getReverseGeocodingData(lat:any, lng:any) {
-//     let latlng = {
-//       lat:lat,
-//       lng:lng
-//     }
-//     // let latlng = new google.maps.LatLng(lat, lng);
-//     let obj:any={ 'latLng': latlng };
-//     // This is making the Geocode request
-//     let geocoder = new google.maps.Geocoder();
-//     geocoder.geocode(obj, function (results, status) {
-//         if (status !== google.maps.GeocoderStatus.OK) {
-//             alert(status);
-//         }
-//         // This is checking to see if the Geoeode Status is OK before proceeding
-//         if (status == google.maps.GeocoderStatus.OK) {
-//             console.log(results);
-//             var address = (results[0].formatted_address);
-//         }
-//     });
-// }
-// get() {  
-//   if (navigator.geolocation) {  
-//       navigator.geolocation.getCurrentPosition((position) => {  
-//           if (position) {  
-//               this.lat = position.coords.latitude;  
-//               this.lng = position.coords.longitude;  
-//               this.getAddress = (this.lat, this.lng)  
-//               console.log(position)  
-//               this.apiloader.load().then(() => {  
-//                   let geocoder = new google.maps.Geocoder;  
-//                   let latlng = {  
-//                       lat: this.lat,  
-//                       lng: this.lng  
-//                   };  
-//                   geocoder.geocode({  
-//                       'location': latlng  
-//                   }, (results) => {  
-//                       if (results[0]) {  
-//                           this.currentLocation = results[0].formatted_address;  
-//                           console.log(this.assgin);  
-//                       } else {  
-//                           console.log('Not found');  
-//                       }  
-//                   });  
-//               });  
-//           }  
-//       })  
-//   }  
-// }  
-// private setCurrentLocation() {
-//   if ('geolocation' in navigator) {
-//     navigator.geolocation.getCurrentPosition((position) => {
-//       this.lat = position.coords.latitude;
-//       this.lng = position.coords.longitude;
-    
-//       this.getAddress(this.lat, this.lng);
-//     });
-//   }
-// }
+  // get() {  
+  //   if (navigator.geolocation) {  
+  //       navigator.geolocation.getCurrentPosition((position) => {  
+  //           if (position) {  
+  //               this.lat = position.coords.latitude;  
+  //               this.lng = position.coords.longitude;  
+  //               this.getAddress = (this.lat, this.lng)  
+  //               console.log(position)  
+  //               this.apiloader.load().then(() => {  
+  //                   let geocoder = new google.maps.Geocoder;  
+  //                   let latlng = {  
+  //                       lat: this.lat,  
+  //                       lng: this.lng  
+  //                   };  
+  //                   geocoder.geocode({  
+  //                       'location': latlng  
+  //                   }, (results) => {  
+  //                       if (results[0]) {  
+  //                           this.currentLocation = results[0].formatted_address;  
+  //                           console.log(this.assgin);  
+  //                       } else {  
+  //                           console.log('Not found');  
+  //                       }  
+  //                   });  
+  //               });  
+  //           }  
+  //       })  
+  //   }  
+  // }  
+  // private setCurrentLocation() {
+  //   if ('geolocation' in navigator) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       this.lat = position.coords.latitude;
+  //       this.lng = position.coords.longitude;
 
-// getAddress(latitude: any, longitude: any) {
-//   this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results: { formatted_address: string | undefined; }[], status: string) => {
-//     if (status === 'OK') {
-//       if (results[0]) {
-     
-//         this.address = results[0].formatted_address;
-//         console.log(this.addres);
-//       } else {
-//         window.alert('No results found');
-//       }
-//     } else {
-//       window.alert('Geocoder failed due to: ' + status);
-//     }
-  
-//   });
-// }
+  //       this.getAddress(this.lat, this.lng);
+  //     });
+  //   }
+  // }
+
+  // getAddress(latitude: any, longitude: any) {
+  //   this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results: { formatted_address: string | undefined; }[], status: string) => {
+  //     if (status === 'OK') {
+  //       if (results[0]) {
+
+  //         this.address = results[0].formatted_address;
+  //         console.log(this.addres);
+  //       } else {
+  //         window.alert('No results found');
+  //       }
+  //     } else {
+  //       window.alert('Geocoder failed due to: ' + status);
+  //     }
+
+  //   });
+  // }
 
 }
